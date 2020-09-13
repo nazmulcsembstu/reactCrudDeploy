b20f1f5c21787c688ea155a417529a2db8dd87cf
unknown <cse14009@gmail.com>
2020-09-13 20:49:55 +0600

    Updates

---
+++ precache-manifest.d8026ea7654613fd9a3245cd0d521c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f1757dfcc083b3b3c599f7356cdad8",
+    "url": "/reactCrudDeploy/index.html"
+  },
+  {
+    "revision": "ea85a68c2fc134be4304",
+    "url": "/reactCrudDeploy/static/css/main.e22e0bb3.chunk.css"
+  },
+  {
+    "revision": "155290e092b70cfbcde7",
+    "url": "/reactCrudDeploy/static/js/2.ffd0a698.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactCrudDeploy/static/js/2.ffd0a698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea85a68c2fc134be4304",
+    "url": "/reactCrudDeploy/static/js/main.eb72d0e6.chunk.js"
+  },
+  {
+    "revision": "db56d1cc5589ffc50318",
+    "url": "/reactCrudDeploy/static/js/runtime-main.12e1bb28.js"
+  }
+]);
